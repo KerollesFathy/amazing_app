14478fae7c504e0fa6b94910be048dd25da06e48
KerollesFathy <kerolles.f@outlook.com>
2023-09-19 22:28:27 +0300

    learn more about dialog api

--- amazing_app/public/js/customer.js
+++ amazing_app/public/js/customer.js
@@ -29,5 +29,60 @@ frappe.ui.form.on("Customer", {
 				}
 			})
 		}, "Group of buttons")
+
+		// new custom button
+
+		frm.add_custom_button("Make VIP Customer via Dialog", () => {
+			let dialog = new frappe.ui.Dialog({
+				title: 'Create VIP Customer',
+				fields: [
+					{
+						label: 'First Name',
+						fieldname: 'first_name',
+						fieldtype: 'Data',
+						reqd: true,
+					},
+					{
+						label: 'Last Name',
+						fieldname: 'last_name',
+						fieldtype: 'Data',
+						reqd: true,
+					},
+					{
+						label: 'User Remark',
+						fieldname: 'user_remark',
+						fieldtype: 'Long Text'
+					}
+				],
+				size: 'large', // small, large, extra-large 
+				primary_action_label: 'Submit',
+				primary_action(values) {
+				console.log(values);
+				frappe.call({
+    			method: 'amazing_app.utils.make_vip_customer_via_dialog',
+				args: {
+					first_name: values.first_name,
+					last_name: values.last_name,
+					user_remark: values.user_remark
+				},
+				// disable the button until the request is completed
+				btn: $('.primary-action'),
+				// freeze the screen until the request is completed
+				freeze: true,
+				callback: (r) => {
+					// on success
+		
+				},
+				error: (r) => {
+					// on error
+				}
+			})
+					dialog.hide();
+				}
+			});
+
+			dialog.show();
+
+		})
 	}
 });
--- amazing_app/utils.py
+++ amazing_app/utils.py
@@ -7,3 +7,13 @@ def make_vip_customer(customer_name):
 	vip_customer.insert(ignore_permissions=True)
 	print(">>>>>>>>>>>>>>>>", vip_customer.name)
 	return vip_customer.name
+
+@frappe.whitelist()
+def make_vip_customer_via_dialog(first_name, last_name, user_remark):
+	frappe.get_doc({
+		"doctype": "VIP Customer",
+		"first_name": first_name,
+		"last_name": last_name,
+		"user_remark": user_remark,
+	}).insert(ignore_permissions=True)
+	frappe.msgprint("created successfuly", alert=True, indicator="green")
