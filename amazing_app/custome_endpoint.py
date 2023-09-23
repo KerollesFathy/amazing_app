08cfeb4270fc85a097a62808d125192abfeda3e7
KerollesFathy <kerolles.f@outlook.com>
2023-09-23 15:22:27 +0300

    added custome endpoint & template files

---
+++ amazing_app/custome_endpoint.py
@@ -0,0 +1,20 @@
+import frappe
+import json
+
+@frappe.whitelist()
+def get_vip_customers(fields):
+	return frappe.db.get_list("VIP Customer",
+	fields=fields,
+	as_list=False)
+
+@frappe.whitelist()
+def recive_data_from_outside(data=None):
+	data = frappe.request.data
+	data_to_dict = json.loads(data)
+
+	print(data_to_dict)
+
+	vip_customer = frappe.new_doc("VIP Customer")
+	vip_customer.first_name = data_to_dict.get("first_name")
+	vip_customer.insert()
+	print(f"\n\n{data}\n\n")
